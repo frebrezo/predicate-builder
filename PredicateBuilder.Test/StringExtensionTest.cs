086667d76efbb1fd79661dcac52c2f1303619cb1
Won Kim <kimwon@iit.edu>
2021-04-11 23:43:23 -0700

    Added string Truncate extension method.

--- PredicateBuilder.Test/StringExtensionTest.cs
+++ PredicateBuilder.Test/StringExtensionTest.cs
@@ -1,6 +1,49 @@
-﻿namespace PredicateBuilder.Test
+﻿using NLipsum.Core;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace PredicateBuilder.Test
 {
     public class StringExtensionTest
     {
+        private readonly Random _random = new Random(Environment.TickCount);
+        private readonly LipsumGenerator _ipsumGen = new LipsumGenerator();
+
+        [Test]
+        public void TruncateTest()
+        {
+            string s = "The quick brown fox jumps over the lazy dog.";
+            string actual = s.Truncate(10);
+
+            Assert.AreEqual("The quick ", actual);
+        }
+
+        [Test]
+        public void Truncate_NoTruncateTest()
+        {
+            string s = "The quick brown fox jumps over the lazy dog.";
+            string actual = s.Truncate(s.Length + 10);
+
+            Assert.AreEqual(s, actual);
+        }
+
+        [Test]
+        public void Truncate_ZeroLengthTest()
+        {
+            string s = "The quick brown fox jumps over the lazy dog.";
+            string actual = s.Truncate(0);
+
+            Assert.AreEqual(string.Empty, actual);
+        }
+
+        [Test]
+        public void Truncate_EmptyStringAndZeroLengthTest()
+        {
+            string s = "";
+            string actual = s.Truncate(0);
+
+            Assert.AreEqual(string.Empty, actual);
+        }
     }
 }
--- PredicateBuilder/StringExtension.cs
+++ PredicateBuilder/StringExtension.cs
@@ -1,8 +1,14 @@
-﻿using System.Text;
-
-namespace PredicateBuilder
+﻿namespace PredicateBuilder
 {
     public static class StringExtension
     {
+        public static string Truncate(this string s, int len)
+        {
+            if (string.IsNullOrEmpty(s)) return s;
+            if (len < 0) return s;
+
+            if (s.Length > len) return s.Substring(0, len);
+            return s;
+        }
     }
 }
